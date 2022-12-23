df3624069955564e1029829c186101d908b61d77
Dekardexe <75323092+Dekardexe@users.noreply.github.com>
2022-12-23 20:53:42 +0300

    ji est

---
+++ teamcity/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace teamcity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            tripleFactorial(5);
+        }
+
+        static void tripleFactorial(int a)
+        {
+            var factorial = new BigInteger(1);
+            for (int i = 1; i <= a; i++)
+            {
+                factorial *= i;
+            }
+            factorial *= 3;
+            Console.WriteLine("Factorial *3 =");
+            Console.Write(factorial) ;
+        }
+
+        
+    }
+}
